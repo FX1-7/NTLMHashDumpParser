010803704f7752429bd36b928eb772da559e97b8
FX17 <31273773+Keiranos@users.noreply.github.com>
2022-12-27 12:15:49 +0000

    Fix first line save issue

--- ParseNTML.py
+++ ParseNTML.py
@@ -1,5 +1,5 @@
 import csv
-f = open("NTLMHashes.txt", "a")
+f = open("NTLMHashes.txt", "w")
 with open(input("What is the file path you wish to open? "), 'r') as file:
     reader = csv.reader(file, delimiter=input("How do you want to split your string? "))
     for row in reader:
